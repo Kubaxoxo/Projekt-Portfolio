2d2e7a4d27a7dad548766f06b10374517c0a4123
Kubaxxxx <kubakruger1@gmail.com>
2022-02-02 09:24:08 +0100

    Projekt Portfolio

---
+++ code/script.js
@@ -0,0 +1,47 @@
+$(function() {
+    $(document).ready(function() {
+    $("#card-cont1, .card-image1").click(function() {
+    $(".card-image1").slideToggle(800);
+    });
+  });
+});
+
+    $(function() {
+    $(document).ready(function() {
+    $("#card-cont2, .card-image2").click(function() {
+    $(".card-image2").slideToggle(800);
+    });
+  });
+});
+  
+    $(function() {
+    $(document).ready(function() {
+    $("#card-cont3, .card-image3").click(function() {
+    $(".card-image3").slideToggle(800);
+    });
+  });
+});
+
+    $(function() {
+    $(document).ready(function() {
+    $("#card-cont4, .card-image4").click(function() {
+    $(".card-image4").slideToggle(800);
+    });
+  });
+});
+    
+    $(function() {
+    $(document).ready(function() {
+    $(".proje").click(function() {
+    $(".pro").slideToggle(800);
+    });
+  });
+});
+    
+    $(function() {
+    $(document).ready(function() {
+    $(".proje").click(function() {
+    $(".pro2").slideToggle(800);
+    });
+  });
+});
